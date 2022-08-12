c4cf422453220af66cc38c9c78ba4bf02a0504f9
Kavin-Velusamy <kavinvelusamy01@gmail.com>
2022-08-12 11:33:42 +0530

    changes

--- basic.py
+++ basic.py
@@ -7,13 +7,32 @@
 
 from calendar import c
 
-
+'''
 print ("hello world")    # print()- inbulit function
 name="kavin22012004"     # name= variable (we can store the values)  # string ("set of character")-command 
 print("Hi",name)
-name="john"
+name="john" 
 print ("hi",name )
 price=45
 amount=34.5              # float value
-child= "false"           # booleam
+child = False           # booleam
 print(child)
+'''
+
+
+code="python learning"
+work='"string"'
+name="in github"
+print(work)
+print(code.upper())
+print(code.lower())
+print(code.capitalize())
+print(code.title())
+print(code + name + work)
+print(code+" "+name)
+print("python \n learning")
+print("python \t learning")
+
+
+
+
