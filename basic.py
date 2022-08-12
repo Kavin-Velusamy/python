075381f3ea4abf2f070d4d3e0137f401016e8e64
Kavin-Velusamy <kavinvelusamy01@gmail.com>
2022-08-12 16:56:01 +0530

    changes

--- basic.py
+++ basic.py
@@ -1,38 +1,72 @@
 #Basic of python
+int=3534654
+string="value"
+float=34.44     #decimal value
+List=[" "]      #list inside string
+Tuple=()
 
 
 
-
-
-
-from calendar import c
+#String=Tuple= immutable(we cannot change the value)
+#List=mutable
 
 '''
+###variable 
+
 print ("hello world")    # print()- inbulit function
 name="kavin22012004"     # name= variable (we can store the values)  # string ("set of character")-command 
 print("Hi",name)
 name="john" 
 print ("hi",name )
 price=45
-amount=34.5              # float value
-child = False           # booleam
+amount=34.5             #float value
+child = False           #booleam
 print(child)
 '''
+
+'''
+###string handling
 
 
 code="python learning"
 work='"string"'
 name="in github"
 print(work)
-print(code.upper())
-print(code.lower())
-print(code.capitalize())
-print(code.title())
-print(code + name + work)
-print(code+" "+name)
-print("python \n learning")
-print("python \t learning")
-print(code.isupper())        # if all the letter is upper(true)
+print(code.upper())         # fully captial letter 
+print(code.lower())         #fully small letter
+print(code.capitalize())    #first letter capital
+print(code.title())         #firat letter and space after the letter is capital
+print(code + name + work)   #connecting the words or numbers
+print(code+" "+name)        #space of the words
+print("python \n learning") 
+print("python \t learning") #giving the the double space of the words
+print(code.isupper())        #if all the letter is upper(true)
 print(code.islower())        #if all the letter is lower ()
 print(len(code))             #finding the index of the word
-print(code.find("n"))
+print(code.find("n"))        #finding the letter (n)
+print(code.count("n "))      #counting the letter (n)
+print(code.replace("a","s")) #replacing the words
+print(code.isalpha())        #finding all the the letter is alpha letter
+print(code.isdigit())
+print(code*5 )              
+
+'''
+### multiple assignment in single line
+name,age ,weight="kavin",19,52    #types of variable in one line
+print(weight)
+like=dislike=100
+print(dislike)
+print(like/dislike)
+
+
+###Type casting
+
+
+
+
+
+
+
+
+
+
