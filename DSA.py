d011145ea167e5991cd979a1ebddfc1bc4b14769
Kavin-Velusamy <kavinvelusamy01@gmail.com>
2022-08-21 16:48:22 +0530

    changes

--- DSA.py
+++ DSA.py
@@ -66,9 +66,9 @@
 
 # stack=[]
 # def push():
-# #     if len(stack)==n:
-# #         print("List is full")
-# #      else:
+#     #  if len(stack)==n:
+#     #      print("List is full")
+#     # else:
 #         element=input("Enter the element:")
 #         stack.append(element)
 #         print(stack)
@@ -79,7 +79,7 @@
 #         e=stack.pop()
 #         print("removed element",e)
 #         print(stack)
-# # n=int(input("Enter the limit"))        # this step for limit in stack 
+#  ##n=int(input("Enter the limit"))        # this step for limit in stack 
 # while True:
 #     print("Selected the operation 1.push 2.pop 3.quit")
 #     choice=int(input())
@@ -124,23 +124,23 @@
 
 # ###sum of element in array
 
-n=int(input("Enter the Limit:"))
-a=[]
-sum=0
-print("Enter the element")
-for i in range (n):
-    a.append(int(input()))
+# n=int(input("Enter the Limit:"))
+# a=[]
+# sum=0
+# print("Enter the element")
+# for i in range (n):
+#     a.append(int(input()))
 
-for i in range(n):   
-    sum+=a[i]
-print("sum of array",sum)
+# for i in range(n):   
+#     sum+=a[i]
+# print("sum of array",sum)
     
     
 ### Queue 
 ### First in First out(FIFO)
 ### Last in last out (LILO)
 ## enqueue = adding the element in first queue
-## dequeue = removing the element in last queue
+## dequeue = removing the element in first queue
 
 # queue=[]
 # queue.append(10)
@@ -155,12 +155,15 @@ print("sum of array",sum)
 # queue.pop()       ## this will remove in last element
 # print(queue)
 
-#enqueue
+##enqueue
 # queue=[]
 # queue.insert(0,10)
 # queue.insert(0,20)
 # queue.insert(0,30)
 # print(queue)
+
+### queue using list
+
 # queue=[]
 # def enqueue():
 #     element=int(input("Enter the element :"))
@@ -188,6 +191,116 @@ print("sum of array",sum)
 #         break
 #     else:
 #         print("Enter the correct operation")
+
+# ## queue usind class
+
+# import collections
+# q= collections.deque()
+# print(q)
+# q.append(10)
+# q.append(20)
+# q.append(30)
+# q.append(40)
+# print(q)
+# q.pop()
+# print(q)
+# q.pop()
+# print(q)
+
+
+''''''
+# # class array_queue:
+#     def __init__(self):
+#         self.capacity=10
+#         self.size=0
+#         self.data=[None]*4
+
+#     def len (self):
+#         return self.size()
+
+#     def _isempty (self):
+#         return self.size==0
+
+#     def enqueue (self,element):
+#         element=int(input("Enter the number"))
+#         self.append(element)
+''''''
+
+### Bubble Sorting
+
+# a,b=3,6           ### ex simple method
+# print(a)
+# temp=a
+# a=b
+# b=temp
+# print(a,b,temp)
+
+
+# def sort(nums):
+#     for i in range (len(nums),-1,0,-1):
+#         for j in range (i):
+#             if nums[j]>nums[j+1]:
+#                 temp=nums[j]
+#                 nums[j]=nums[j+1]
+#                 nums[j+1]=temp
+            
+# nums=[5,3,8,9,1,0,7]
+# sort(nums)
+# print(nums)
+
+## method of bubble sort 
+
+# def bubble_sort(self,list):
+#     for i in range(len(list)-1):
+#         for j in range(len(list)-1):
+#             if list[j]>list[j+1]:
+#                  temp=list[j+1]
+#                  list[j+1]=list[j]
+#                  list[j]=temp
+#         return(list)
+
+### Selection sort
+
+# def sort(nums):
+#     for i in range(5):
+#         minpos =i
+#         for j in range(i,6):
+#             if nums[j]<nums[minpos]:
+#                 minpos=j
+#         temp=nums[i]
+#         nums[i]=nums[minpos]
+#         nums[minpos]=temp
+
+# nums=[5,3,8,6,7,2]
+# sort(nums)
+# print(nums)
+
+##code of selectio
+# n sort 
+# for i in range(5):
+#         minpos =i
+#         for j in range(i,6):
+#             if min[j]<min[minpos]:
+#                 minpos=j
+#         temp=min[i]
+#         min[i]=min[minpos]
+#         min[minpos]=temp
+
+# ### Insertion Sort
+
+arr=[2,4,7,8,0,5,45]
+
+for i in range (len(arr)):
+    cur=arr[i]
+    for j in range (i-1,-1,-1):
+        if arr[j]>cur:
+            arr[j+1]=arr[j]
+        else:
+            arr[j+1]=cur
+            break
+for i in range (0,len(arr)):
+    print(arr[i],end=' ')       
+
 
 
 
