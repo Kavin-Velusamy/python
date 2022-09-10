9ad22bd5d1bb5f9b8b73653ef68566d8a7185f90
Kavin-Velusamy <kavinvelusamy01@gmail.com>
2022-09-10 13:36:42 +0530

    cxc

--- sudoko.py
+++ sudoko.py
@@ -1,61 +1,61 @@
-# M = 9
-# def puzzle(a):
-#     for i in range(M):
-#         for j in range(M):
-#             print(a[i][j],end = " ")
-#         print()
-# def solve(grid, row, col, num):
-#     for x in range(9):
-#         if grid[row][x] == num:
-#             return False
+M = 9
+def puzzle(a):
+    for i in range(M):
+        for j in range(M):
+            print(a[i][j],end = " ")
+        print()
+def solve(grid, row, col, num):
+    for x in range(9):
+        if grid[row][x] == num:
+            return False
              
-#     for x in range(9):
-#         if grid[x][col] == num:
-#             return False
+    for x in range(9):
+        if grid[x][col] == num:
+            return False
  
  
-#     startRow = row - row % 3
-#     startCol = col - col % 3
-#     for i in range(3):
-#         for j in range(3):
-#             if grid[i + startRow][j + startCol] == num:
-#                 return False
-#     return True
+    startRow = row - row % 3
+    startCol = col - col % 3
+    for i in range(3):
+        for j in range(3):
+            if grid[i + startRow][j + startCol] == num:
+                return False
+    return True
  
-# def Suduko(grid, row, col):
+def Suduko(grid, row, col):
  
-#     if (row == M - 1 and col == M):
-#         return True
-#     if col == M:
-#         row += 1
-#         col = 0
-#     if grid[row][col] > 0:
-#         return Suduko(grid, row, col + 1)
-#     for num in range(1, M + 1, 1): 
+    if (row == M - 1 and col == M):
+        return True
+    if col == M:
+        row += 1
+        col = 0
+    if grid[row][col] > 0:
+        return Suduko(grid, row, col + 1)
+    for num in range(1, M + 1, 1): 
      
-#         if solve(grid, row, col, num):
+        if solve(grid, row, col, num):
          
-#             grid[row][col] = num
-#             if Suduko(grid, row, col + 1):
-#                 return True
-#         grid[row][col] = 0
-#     return False
+            grid[row][col] = num
+            if Suduko(grid, row, col + 1):
+                return True
+        grid[row][col] = 0
+    return False
  
-# '''0 means the cells where no value is assigned'''
-# grid = [[2, 5, 0, 0, 3, 0, 9, 0, 1],
-#         [0, 1, 0, 0, 0, 4, 0, 0, 0],
-#     [4, 0, 7, 0, 0, 0, 2, 0, 8],
-#     [0, 0, 5, 2, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 9, 8, 1, 0, 0],
-#     [0, 4, 0, 0, 0, 3, 0, 0, 0],
-#     [0, 0, 0, 3, 6, 0, 0, 7, 2],
-#     [0, 7, 0, 0, 0, 0, 0, 0, 3],
-#     [9, 0, 3, 0, 0, 0, 6, 0, 4]]
+'''0 means the cells where no value is assigned'''
+grid = [[2, 5, 0, 0, 3, 0, 9, 0, 1],
+        [0, 1, 0, 0, 0, 4, 0, 0, 0],
+    [4, 0, 7, 0, 0, 0, 2, 0, 8],
+    [0, 0, 5, 2, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 9, 8, 1, 0, 0],
+    [0, 4, 0, 0, 0, 3, 0, 0, 0],
+    [0, 0, 0, 3, 6, 0, 0, 7, 2],
+    [0, 7, 0, 0, 0, 0, 0, 0, 3],
+    [9, 0, 3, 0, 0, 0, 6, 0, 4]]
  
-# if (Suduko(grid, 0, 0)):
-#     puzzle(grid)
-# else:
-#     print("Solution does not exist:(")
+if (Suduko(grid, 0, 0)):
+    puzzle(grid)
+else:
+    print("Solution does not exist:(")
 
 
 
@@ -80,21 +80,21 @@
 
 # ### print table of a number accepted from user.
 
-# num=int(input("Enter the table"))
+# num=int(input("Enter the table :"))
 # for i in range(1,11):
-#     print(num*i)
+#     print(num*i,end=',')
 
 ### Write a program to display product of the digits of a number accepted from the user.
 
-num=int(input("Enter the number :"))
-p=1
-while(num):
-    r=num%10                                              ##dout
-    p=p*r
-    num=num//10
-    print("print the product of digit is:",p)
+# num=int(input("Enter the number :"))
+# p=1
+# while(num):
+#     r=num%10                                              ##dout
+#     p=p*r
+#     num=num//10
+#     print("print the product of digit is:",p)
 
-###Write a program to find the factorial of a number.
+### Write a program to find the factorial of a number.
 
 # num=int(input("Enter te number :"))
 # f=1
@@ -115,6 +115,8 @@ while(num):
 
 
 ###Write a program to check whether a number is prime or not.
+ 
+num=int(input("Enter the number :"))
 
 
 
